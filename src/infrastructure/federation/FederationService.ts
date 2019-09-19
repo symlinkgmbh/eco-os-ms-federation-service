2347bc272dbba37c40f98edc60f5be765a6eebed
Symlink GmbH <info@symlink.de>
2019-09-19 13:40:14 +0000

    release
    Signed-off-by: Symlink GmbH <info@symlink.de>

--- src/infrastructure/federation/FederationService.ts
+++ src/infrastructure/federation/FederationService.ts
@@ -1,19 +1,21 @@
-/** 
-* Copyright 2018-2019 Symlink GmbH 
-* 
-* Licensed under the Apache License, Version 2.0 (the "License"); 
-* you may not use this file except in compliance with the License. 
-* You may obtain a copy of the License at 
-*  
-*     http://www.apache.org/licenses/LICENSE-2.0 
-* 
-* Unless required by applicable law or agreed to in writing, software 
-* distributed under the License is distributed on an "AS IS" BASIS, 
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
-* See the License for the specific language governing permissions and 
-* limitations under the License. 
-* 
-*/ 
+/**
+ * Copyright 2018-2019 Symlink GmbH
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
+
+
 
 import { IFederationService } from "./IFederationService";
 import dns from "dns";
@@ -83,8 +85,7 @@ export class FederationService implements IFederationService {
         );
       }
 
-      const result = await this.federationRequest(federationObject[index].publickey, email, domain, "localhost:9000");
-      // await this.federationRequest(federationObject[index].publickey, email, domain, federationObject[index].srv[0].name);
+      const result = await this.federationRequest(federationObject[index].publickey, email, domain, federationObject[index].srv[0].name + ":" + federationObject[index].srv[0].port);
       return result.data;
     }
   }
@@ -111,7 +112,7 @@ export class FederationService implements IFederationService {
       for (const dIndex in response.data[index]) {
         const dnsLookupResult = await this.resolve2ndLock(response.data[index][dIndex].domain);
         if (dnsLookupResult !== null) {
-          await parsedResult.push({
+          parsedResult.push({
             domain: response.data[index][dIndex].domain,
             created: String(new Date().getTime()),
             publickey: response.data[index][dIndex].publicKey,
